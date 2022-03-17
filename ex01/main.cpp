3a02e800bef82c56f648d7d4e34cf539f6a94c35
anduuin <jcm2232@gmail.com>
2022-03-17 14:04:52 +0900

    Improve testcase

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: cjeon <cjeon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/16 15:35:48 by cjeon             #+#    #+#             */
-/*   Updated: 2022/03/17 13:55:26 by cjeon            ###   ########.fr       */
+/*   Updated: 2022/03/17 14:04:39 by cjeon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ int main(void) {
   {
     ScavTrap c("TEST");
 
-    for (int i = 0; i < 10; i++) c.attack("someone");
+    for (int i = 0; i < 50; i++) c.attack("someone");
     c.attack("someone");
 
     for (int i = 0; i < 10; i++) c.takeDamage(10);
